0ec07bb049b3b8aaf9ea58372204730e11c68911
Kastriot Limani <kasskelimani@gmail.com>
2024-10-27 00:18:16 +0200

    made it a progressive web app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Buying List App PWA',
+        short_name: 'BL PWA',
+        description: 'My awesome buying list Progressive Web App!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icon512_maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon512_rounded.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
+  ],
+});
